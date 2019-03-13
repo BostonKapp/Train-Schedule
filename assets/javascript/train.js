9e3261fc120fccc51ae2606e6b779bbb7dc709aa
Devon Galloway <Devon_Galloway5@hotmail.com>
2019-03-13 13:00:04 -0400

    Firebase added

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,1 +1,11 @@
-var train = "train1"
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBf87E4zr1_e7bmnTrY4E5LQm9rY3B3nQE",
+    authDomain: "train-schedule-634c1.firebaseapp.com",
+    databaseURL: "https://train-schedule-634c1.firebaseio.com",
+    projectId: "train-schedule-634c1",
+    storageBucket: "train-schedule-634c1.appspot.com",
+    messagingSenderId: "513111606911"
+  };
+  firebase.initializeApp(config);
